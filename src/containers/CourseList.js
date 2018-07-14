56e1206e390aba355021ab4e8a913e44e048ca26
Akriti <akriti.bhat28@gmail.com>
2018-07-13 20:39:45 -0400

    New Pom

---
+++ src/containers/CourseList.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-do'
+import HelloWord from '.hello'
+
+ReactDOM.render(<HelloWorld/>,document.getElementById('root'));
---
+++ src/containers/CourseManager.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-do'
+
+class HelloWorld extends React.Component{
+	render(){
+		return (
+				<h1> Hello World 123!!! </h1>
+				)
+	}
+}
+
+export default HelloWorld;
