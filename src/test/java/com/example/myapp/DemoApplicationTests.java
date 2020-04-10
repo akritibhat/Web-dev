487637ff460777f0a6b687f50d5df0fbfa679f2b
Akriti Bhat <8200097+akritibhat@users.noreply.github.com>
2020-04-10 13:31:17 -0700

    Update DemoApplicationTests.java

--- src/test/java/com/example/myapp/DemoApplicationTests.java
+++ src/test/java/com/example/myapp/DemoApplicationTests.java
@@ -6,11 +6,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration(classes = { Application.class })
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testfindAllHellos() {
+		
+		String body = RestAssured.
+      given().
+        accept(ContentType.JSON).
+      when().
+        get("/api/hello").
+      then().
+        statusCode(HttpStatus.SC_OK).
+        contentType(ContentType.JSON).
+        extract().asString();
+    Assert.assertThat(body, Matchers.equalTo("Retrieved entity with id: blah"));
 	}
 
 }
